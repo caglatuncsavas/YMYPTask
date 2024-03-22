0dcbfb60df5b48364992640bd28b6c96c1aaf039
caglatunc <caglatunc03@gmail.com>
2024-03-23 02:09:48 +0300

    Login işelmi için Application katmanı CQRS Pattern kullanılarak oluşturuldu. Fake register işlemi IoC containera eklendi.

---
+++ YMYPTask.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace YMYPTask.Application.Features.Auth.Login;
+internal sealed class LoginCommandHandler(
+    UserManager<Appuser> userManager) : IRequestHandler<LoginCommand, string>
+{
+    public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? appUser = await userManager.FindByNameAsync(request.UserName);
+
+        if(appUser is null)
+        {
+            throw new Exception("User not found");
+        }
+
+        bool isPasswordTrue = await userManager.CheckPasswordAsync(appUser, request.Password);
+
+        if(!isPasswordTrue)
+        {
+            throw new Exception("Password is wrong");
+        }
+
+        return "User logged in successfully";
+    }
+}
