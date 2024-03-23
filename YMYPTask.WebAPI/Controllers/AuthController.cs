8c943812f466f44ec05bf966bba1ffe23eb4963b
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-03-23 20:16:59 +0300

    Controllers eklendi.

---
+++ YMYPTask.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using YMYPTask.Application.Features.Auth.Login;
+
+namespace YMYPTask.WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class AuthController(
+    IMediator mediator) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return Ok(new { Message = response });
+    }
+}
